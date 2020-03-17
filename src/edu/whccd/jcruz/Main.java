2121c324ea0c893a5a5cbf61d1d2cf7754329cb8
Jonathan Cruz <jcruz@2cruz.com>
2020-03-16 23:38:29 -0700

    Initial commit

---
+++ src/edu/whccd/jcruz/Main.java
@@ -0,0 +1,13 @@
+package edu.whccd.jcruz;
+
+/*
+    The purpose of this application is to display Hello World!
+    author:     Jonathan Cruz
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
